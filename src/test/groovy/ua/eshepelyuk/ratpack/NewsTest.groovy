8bee3278ba9d9645e3f69ef59a118f043306fcd0
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2016-02-06 16:11:12 +0200

    Unit tests

--- src/test/groovy/ua/eshepelyuk/ratpack/NewsTest.groovy
+++ src/test/groovy/ua/eshepelyuk/ratpack/NewsTest.groovy
@@ -1,10 +1,12 @@
 package ua.eshepelyuk.ratpack
 
-import groovy.json.JsonOutput
+import com.fasterxml.jackson.core.JsonParseException
 import io.netty.handler.codec.http.HttpResponseStatus
 import ratpack.jackson.JsonRender
+import spock.lang.Ignore
 import spock.lang.Specification
 
+import static groovy.json.JsonOutput.toJson
 import static io.netty.handler.codec.http.HttpResponseStatus.NOT_ACCEPTABLE
 import static ratpack.groovy.test.handling.GroovyRequestFixture.handle
 import static ratpack.handling.internal.DefaultByContentSpec.TYPE_JSON
@@ -44,7 +46,7 @@ class NewsTest extends Specification {
         given:
         def items = [createItem(1L)]
         and:
-        0 * newsItemDAO.findAll() >> items
+        0 * newsItemDAO.findAll()
 
         when: "using unsupported content type"
         def response = handle(newsItemActionChain) {
@@ -76,7 +78,7 @@ class NewsTest extends Specification {
     def "should Accept Only JSON For Find Single News"() {
         given:
         NewsItem item = createItem(2L)
-        0 * newsItemDAO.findById(2L) >> item
+        0 * newsItemDAO.findById(2L)
 
         when: "using unsupported content type"
         def response = handle(newsItemActionChain) {
@@ -128,7 +130,7 @@ class NewsTest extends Specification {
         def response = handle(newsItemActionChain) {
             uri ""
             method "POST"
-            body(JsonOutput.toJson(item), TYPE_JSON)
+            body(toJson(item), TYPE_JSON)
             header "Accept", TYPE_JSON
         }
 
@@ -136,44 +138,54 @@ class NewsTest extends Specification {
         response.bodyText as Long == 444L
     }
 
-//    @Test
-//    public void shouldAcceptOnlyJSONForAddingNews() {
-//        given:
-//        NewsItem item = createItemWithoutId();
-//        when(itemsDao.insert(any(NewsItem.class))).thenReturn(444L);
-//
-//        when: using unsupported content type
-//        Response response = NEWS.request(APPLICATION_ATOM_XML).post(json(item));
-//
-//        then: DAO not called and 4XX returned
-//        verifyZeroInteractions(itemsDao);
-//        assertThat(response.getStatus()).isEqualTo(NOT_ACCEPTABLE.getStatusCode());
-//    }
-//
-//    @Test
-//    public void shouldAcceptOnlyProperJSONForAddingNews() {
-//        given:
-//        when(itemsDao.insert(any(NewsItem.class))).thenReturn(444L);
-//
-//        when: posting arbitrary data instead of JSON
-//        Response response = NEWS.request(APPLICATION_JSON_TYPE).post(text("test string"));
-//
-//        then: DAO not called and 4XX returned
-//        verifyZeroInteractions(itemsDao);
-//        assertThat(response.getStatus()).isEqualTo(UNSUPPORTED_MEDIA_TYPE.getStatusCode());
-//    }
-//
-//    @Test
-//    public void shouldValidateItemWhenAddingNews() {
-//        given: item violating validation
-//        NewsItem item = new NewsItem(null, "author5", "content5", new Date());
-//        when(itemsDao.insert(any(NewsItem.class))).thenReturn(555L);
-//
-//        when:
-//        Response response = NEWS.request(APPLICATION_JSON_TYPE).post(json(item));
-//
-//        then: DAO not called and 4XX returned
-//        verifyZeroInteractions(itemsDao);
-//        assertThat(response.getStatus()).isEqualTo(422);
-//    }
+    def "should Accept Only JSON For Adding News"() {
+        given:
+        NewsItem item = createItemWithoutId()
+        0 * newsItemDAO.insert(_)
+
+        when: "using unsupported content type"
+        def response = handle(newsItemActionChain) {
+            uri ""
+            method "POST"
+            body(toJson(item), TYPE_XML)
+            header "Accept", TYPE_XML
+        }
+
+        then: "DAO not called and 4XX returned"
+        response.status.nettyStatus == NOT_ACCEPTABLE
+    }
+
+    def "should Accept Only Proper JSON For Adding News"() {
+        given:
+        0 * newsItemDAO.insert(_)
+
+        when: "posting arbitrary data instead of JSON"
+        def response = handle(newsItemActionChain) {
+            uri ""
+            method "POST"
+            body("test string", TYPE_JSON)
+            header "Accept", TYPE_JSON
+        }
+
+        then: "DAO not called and 4XX returned"
+        response.exception(JsonParseException) != null
+    }
+
+    @Ignore
+    def "should Validate Item When Adding News"() {
+        given: "item violating validation"
+        NewsItem item = new NewsItem(null, null, "author5", "content5", new Date());
+        0 * newsItemDAO.insert(_)
+
+        when:
+        def response = handle(newsItemActionChain) {
+            uri ""
+            method "POST"
+            body(toJson(item), TYPE_JSON)
+            header "Accept", TYPE_JSON
+        }
+
+        then: "DAO not called and 4XX returned"
+        response.status.code == 422
+    }
 }
