a6bd727473ec73a29dd23fe56e774050bb1c6da1
Evgeny Shepelyuk <eshepelyuk@gmail.com>
2016-03-15 14:43:28 +0200

    Basic Gatling integration

---
+++ src/test/scala/simulation/BasicSimulation.scala
@@ -0,0 +1,21 @@
+package simulation
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:5050")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario("BasicSimulation").exec(http("GET /news").get("/news"))
+
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
